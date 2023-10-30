50aca85a42aa92e22df17658d46a60379ecb223d
Angelina Durigan <143005728+AngelDurigan@users.noreply.github.com>
2023-10-30 01:22:23 +0000

    criando validadores de cliente

--- api/src/database/dba/cliente.js
+++ api/src/database/dba/cliente.js
@@ -15,4 +15,33 @@ module.exports = class Cliente {
         this.senha = senha
         this.urlAvatar = urlAvatar
     }
+
+    static validaId = (data) => (
+        !data && typeof data != "number" &&
+        data <= 0
+    )
+    static validaCargo = (data) => (
+        !data && typeof data != "string" &&
+        data.length < 15 && data.lenght > 0
+    )
+    static validaNome = (data) => (
+        !nome && typeof data != "string" &&
+        data.length < 50 && data.length > 3
+    )
+    static validaEmail = (data) => (
+        !data && typeof data != "string" &&
+        data.length < 50 && data.length > 0
+    )
+    static validaEndereco = (data) => (
+        !data && typeof data != "string"  &&
+        data.length < 50 && data.length > 0
+    )
+    static validaSenha = (data) => (
+        !data && typeof data != "string" &&
+        data.length > 8
+    )
+    static validaUrlAvatar = (data) => (
+        !data && typeof data != "string" &&
+        data.length > 0
+    )
 }
