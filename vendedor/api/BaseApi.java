0d39d57cea26d34a88de9e49a44717e5276c18e5
Angelina Durigan <143005728+AngelDurigan@users.noreply.github.com>
2023-10-31 22:40:42 +0000

    iniciando uma request em java

---
+++ vendedor/api/BaseApi.java
@@ -0,0 +1,5 @@
+package api;
+
+public class BaseApi {
+  public static String BASE_URL = "http://localhost:8080";
+}
---
+++ vendedor/api/utils/IConvertJson.java
@@ -0,0 +1,6 @@
+package api.utils;
+
+public class IConvertJson {
+  public String toJson() { throw new UnsupportedOperationException("is not implemented"); }
+  public static Object fromJson(String json) { throw new UnsupportedOperationException("is not implemented"); }
+}
